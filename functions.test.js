6b678abc7f4a631169a6d3477bb5d4d96f339481
olympiawoj <owojcik7@gmail.com>
2020-07-27 16:38:57 -0400

    toEqual

--- functions.test.js
+++ functions.test.js
@@ -2,10 +2,13 @@ const functions = require('./functions')
 
 // 1st arg: description - anything we want
 // 2nd arg: function
+
+// toBe
 test('Adds 2 + 2 to equal 4', ()=> {
     expect(functions.add(2, 2)).toBe(4)
 })
 
+// not
 test('Adds 2 + 2 to NOT equal 5', ()=> {
     expect(functions.add(2, 2)).not.toBe(5)
 })
@@ -18,10 +21,20 @@ test('Adds 2 + 2 to NOT equal 5', ()=> {
 // toBeTruthy matches anything that an if statement treats as true
 // toBeFalsy matches everything that an if statement treats as false
 
+// toBeNull
 test('Should be null', ()=> {
     expect(functions.isNull()).toBeNull()
 })
 
+// toBeFalsy 
 test('Should be falsy', () => {
-    expect(functions.checkValue(2)).toBeFalsy()
+    expect(functions.checkValue(undefined)).toBeFalsy()
+})
+
+// toEqual
+test('Should be falsy', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Brad',
+        lastName: 'Traversy'
+    })
 })
