514584a1acdc3337355b0fd49b338d35e742fe5d
olympiawoj <owojcik7@gmail.com>
2020-07-28 11:40:28 -0400

    describe - beforeEach

--- functions.test.js
+++ functions.test.js
@@ -3,12 +3,26 @@ const functions = require('./functions')
 // beforeEach(()=> initDatabase())
 // afterEach(()=> closeDatabase())
 
-beforeAll(()=> initDatabase())
-afterAll(()=> closeDatabase())
+// beforeAll(()=> initDatabase())
+// afterAll(()=> closeDatabase())
 
-const initDatabase = ()=> console.log('Database Initializied...')
-const closeDatabase = ()=> console.log('Database Closed...')
+// const initDatabase = ()=> console.log('Database Initializied...')
+// const closeDatabase = ()=> console.log('Database Closed...')
 
+const nameCheck = () => console.log('Checking Name...')
+
+describe('Checking Names', ()=>{
+    beforeEach(()=> nameCheck());
+    test('User is Jeff', ()=> {
+        const user = 'Jeff'
+        expect(user).toBe('Jeff')
+    })
+
+    test('User is Karen', ()=> {
+        const user = 'Karen'
+        expect(user).toBe('Karen')
+    })
+})
 
 // 1st arg: description - anything we want
 // 2nd arg: function
